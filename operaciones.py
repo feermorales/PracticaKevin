31050b1985d881e2807ab502a2920d8a3ca360fb
feermorales <feernando_morales@outlook.es>
2020-03-27 00:24:16 +0000

    Está es la aportación de Feer

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+n1 = float(input("Ingresa el primer número: "))
+n2 = float(input("Ingresa el segundo número: "))
+
+def suma(val1=0, val2=0):
+    return val1 + val2
+
+def operacion(funcion, val1=0, val2=0):
+    return funcion(val1, val2)
+
+funcion_suma = suma
+resultado_sum = operacion(funcion_suma, n1, n2)
+
+print("\nEl resultado de la SUMA es: ", resultado_sum)
