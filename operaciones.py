90b8b70fb0eb673b56eafa892bb596b1afbca058
GiboAdrian <gibbymetal@hotmail.com>
2020-03-26 19:02:09 -0600

    agregamos otra funcion

--- operaciones.py
+++ operaciones.py
@@ -4,10 +4,18 @@ n2 = float(input("Ingresa el segundo número: "))
 def suma(val1=0, val2=0):
     return val1 + val2
 
+def resta(val1=0, val2=0):
+    return val1 - val2
+
 def operacion(funcion, val1=0, val2=0):
     return funcion(val1, val2)
 
 funcion_suma = suma
 resultado_sum = operacion(funcion_suma, n1, n2)
 
+funcion_resta = resta
+resultado_res = operacion(funcion_resta, n1, n2)
+
 print("\nEl resultado de la SUMA es: ", resultado_sum)
+print("\nEl resultado de la RESTA es: ", resultado_res)
+
