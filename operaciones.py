148d1486fcce05b9b2e5842101912a4d9c66e5e0
BereniceSantiago <bere_082598@outlook.com>
2020-03-26 20:41:07 -0600

    agregamos otra funcion

--- operaciones.py
+++ operaciones.py
@@ -7,6 +7,9 @@ def suma(val1=0, val2=0):
 def resta(val1=0, val2=0):
     return val1 - val2
 
+def multi(val1=0, val2=0):
+    return val * val2
+
 def operacion(funcion, val1=0, val2=0):
     return funcion(val1, val2)
 
